d5e4514891a6650d91fd59a24ec133407d952912
Akshay Telang <akshayit22@gmail.com>
2024-05-26 13:40:07 +0530

    like and interested controller created

--- server/controllers/Dashboard.js
+++ server/controllers/Dashboard.js
@@ -30,3 +30,84 @@ exports.dashboard = async(req,res) =>{
 		});
 	}
 }
+
+exports.likeOrDislike = async(req,res) =>{
+	try{
+		const {PropertyId,mode} = req.body;
+
+
+		const {id,email} = req.user;
+		const userDetails = await User.findOne({ email });
+		console.log("In the likeOrDislike controller", req.user.email);
+
+		const PropertyDetails = await Property.findById({PropertyId});
+
+		if (!PropertyId || !mode) {
+			return res.status(403).send({
+				success: false,
+				message: "All Fields are required",
+			});
+		}
+
+		if(mode === "Like"){
+			const saved = await Property.findByIdAndUpdate(
+					{_id:PropertyId},
+					{LikesCount: PropertyDetails.LikesCount + 1},
+					{new:true}
+			)
+		}
+		else if(mode == "Dislike"){
+			const saved = await Property.findByIdAndUpdate(
+				{_id:PropertyId},
+				{LikesCount: PropertyDetails.LikesCount - 1},
+				{new:true}
+		)
+		}		
+
+		return res.status(200).json({
+			success: true,
+			message: "Liked or Unliked successfully",
+		});
+
+
+	}
+	catch(error){
+		console.error(error);
+		return res.status(500).json({
+			success: false,
+			message: "Error in likeOrDislike, Please try again.",
+		});
+	}
+}
+
+exports.intrested = async(req,res) =>{
+	try{
+		const {PropertyId} = req.body;
+
+
+		const {id,email} = req.user;
+		const userDetails = await User.findOne({ email });
+		console.log("In the Intrested controller", req.user.email);
+
+		const saveIntrested = await Intrested.create({
+			user:id,
+			property:PropertyId,
+		})
+						
+
+		return res.status(200).json({
+			success: true,
+			saveIntrested,
+			message: "Intreste saved successfully",
+		});
+
+
+	}
+	catch(error){
+		console.error(error);
+		return res.status(500).json({
+			success: false,
+			message: "Error in Intrested, Please try again.",
+		});
+	}
+}
