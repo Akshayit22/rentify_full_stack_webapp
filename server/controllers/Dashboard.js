ec289b5828e5650d073b195333f0d80d34220329
Akshay Telang <akshayit22@gmail.com>
2024-05-26 13:21:45 +0530

    dashboard controller created

--- server/controllers/Dashboard.js
+++ server/controllers/Dashboard.js
@@ -1,15 +1,22 @@
 const User = require('../models/User');
+const Property = require('../models/Property');
+const Intrested = require('../models/Intrested');
 
 exports.dashboard = async(req,res) =>{
 	try{
 		const {id,email} = req.user;
+		const userDetails = await User.findOne({ email });
 		console.log("In the dashboard controller", req.user.email);
 
 		//like and intrested remain
+		const OwnedProperties = await Property.find({owner:userDetails});
+
+		const IntrestedProperties = await Intrested.find({user:userDetails}).populate('property').exec();
 
 		return res.status(200).json({
 			success: true,
-			
+			OwnedProperties,
+			IntrestedProperties,
 			message: "User details fetched successfully",
 		});
 
